4cd55a3769f2438a0e0b464f238d040b5e0523ad
ZankBF <73807753+ZankBF@users.noreply.github.com>
2020-11-08 23:52:05 -0600

    Add files via upload

--- Zank Week 10.py
+++ Zank Week 10.py
@@ -6,16 +6,15 @@ with open('names.csv', 'w', newline ='')as csvfile:
     fieldnames = ['Name', 'Address', 'Phone']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    
-
     name = input("Please enter your name: ")
     address = input("Please enter your address: ")
     phone = input("Please enter your phone number: ")
-    
+
+    dir
+    Phone = phone
+    Address = address
+    Phone = phone
+
     writer.writeheader()
+    writer.writerow({'Name, Address, Phone'})
 
-
-
-    writer.writerow({str(name), str(address), str(phone)})
-    
-    
